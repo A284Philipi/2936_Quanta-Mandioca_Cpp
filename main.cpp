31535c55282ad571bc086be61d65604a2bda4b2a
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-18 20:56:05 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, r;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    r = (a * 300) + (b * 1500) + (c * 600) + (d * 1000) + (e * 150) + 225;
+    cout << r <<endl;
+    return 0;
+}
